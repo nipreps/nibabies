c4d06a1e3742fbb57843865a0fc3e357c7665dbf
mathiasg <mathiasg@stanford.edu>
2020-10-14 16:10:07 -0400

    ENH: Port over fmriprep main entrypoint

--- nibabies/cli/run.py
+++ nibabies/cli/run.py
@@ -1,166 +1,180 @@
-"""Main runner"""
-import logging
-from pathlib import Path
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""NiBabies runner."""
+from .. import config
 
 
-def get_parser():
-    """Build parser object."""
-    from os import cpu_count
-    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-    from .._version import get_versions
-
-    parser = ArgumentParser(
-        description="""\
-nibabies-be -- Atlas-based brain extraction tool of the \
-ANTs package.\
-""",
-        formatter_class=ArgumentDefaultsHelpFormatter,
-    )
-
-    parser.add_argument(
-        "command",
-        choices=('bew', 'bew+surf'),
-        help="Specific nibabies commandline workflow",
-    )
-    parser.add_argument(
-        "input_image",
-        type=Path,
-        help="The target image for brain extraction.",
-    )
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="nibabies-be v{}".format(get_versions()["version"]),
-    )
-    parser.add_argument(
-        "--template",
-        choices=("MNIInfant", "UNCInfant"),
-        default="UNCInfant",
-        help="The TemplateFlow ID of the reference template.",
-    )
-    parser.add_argument(
-        "--cohort",
-        type=int,
-        choices=range(1,12),
-        help="TemplateFlow cohort ID of the reference template"
-    )
-    parser.add_argument(
-        "--omp-nthreads",
-        type=int,
-        default=cpu_count(),
-        help="Number of CPUs available for multithreading processes.",
-    )
-    parser.add_argument(
-        "--nprocs",
-        type=int,
-        default=cpu_count(),
-        help="Number of processes that can be run in parallel.",
-    )
-    parser.add_argument(
-        "-m",
-        "--mri-scheme",
-        default="T1w",
-        choices=("T1w", "T2w"),
-        help="select a particular MRI scheme",
-    )
-    parser.add_argument(
-        "-o",
-        "--output-dir",
-        type=Path,
-        default=Path("results").absolute(),
-        help="path where intermediate results should be stored",
-    )
-    parser.add_argument(
-        "-w",
-        "--work-dir",
-        type=Path,
-        default=Path("work").absolute(),
-        help="path where intermediate results should be stored",
-    )
-    parser.add_argument(
-        "--sloppy",
-        dest="debug",
-        action="store_true",
-        default=False,
-        help="Use low-quality tools for speed - TESTING ONLY",
-    )
-    parser.add_argument(
-        "--age-months",
-        dest="age_months",
-        type=int,
-        help="Age (in months)",
-    )
-    parser.add_argument(
-        "--subject",
-        dest="subject_id",
-        help="subject ID (if running infant recon-all)"
-    )
-    return parser
-
-
-def main(argv=None):
+def main():
     """Entry point."""
-    from nipype import config
+    from os import EX_SOFTWARE
+    from pathlib import Path
+    import sys
+    import gc
+    from multiprocessing import Process, Manager
+    from .parser import parse_args
+    from ..utils.bids import write_derivative_description, write_bidsignore
 
-    opts = get_parser().parse_args(argv)
-    template_specs = {}
-    if opts.template == 'MNIInfant':
-        template_specs = {'resolution': 2 if opts.debug else 1}
+    parse_args()
 
-    # specify cohort
-    if opts.cohort:
-        template_specs['cohort'] = opts.cohort
-    elif opts.age_months:
-        if opts.age_months <= 2:
-            cohort = 1
-        elif opts.age_months < 12:
-            cohort = 2
-        else:
-            cohort = 3
-        template_specs['cohort'] = cohort
+    sentry_sdk = None
+    # if not config.execution.notrack:
+    #     import sentry_sdk
+    #     from ..utils.sentry import sentry_setup
 
-    if opts.command == 'bew':
-        from ..workflows.anatomical.brain_extraction import init_infant_brain_extraction_wf
-        wf = init_infant_brain_extraction_wf(
-            ants_affine_init=True,
-            debug=opts.debug,
-            in_template=opts.template,
-            template_specs=template_specs,
-            mri_scheme=opts.mri_scheme,
-            omp_nthreads=opts.omp_nthreads,
-            output_dir=opts.output_dir,
-        )
-    elif opts.command == 'bew+surf':
-        from ..workflows.anatomical import init_infant_anat_wf
-        wf = init_infant_anat_wf(
-            template_name=opts.template,
-            template_specs=template_specs,
-            age_months=opts.age_months,
-            mri_scheme=opts.mri_scheme,
-            omp_nthreads=opts.omp_nthreads,
-            output_dir=opts.output_dir,
-            subject_id=opts.subject_id,
-        )
+    #     sentry_setup()
+
+    # CRITICAL Save the config to a file. This is necessary because the execution graph
+    # is built as a separate process to keep the memory footprint low. The most
+    # straightforward way to communicate with the child process is via the filesystem.
+    config_file = config.execution.work_dir / config.execution.run_uuid / "config.toml"
+    config_file.parent.mkdir(exist_ok=True, parents=True)
+    config.to_filename(config_file)
+
+    # CRITICAL Call build_workflow(config_file, retval) in a subprocess.
+    # Because Python on Linux does not ever free virtual memory (VM), running the
+    # workflow construction jailed within a process preempts excessive VM buildup.
+    with Manager() as mgr:
+        from .workflow import build_workflow
+
+        retval = mgr.dict()
+        p = Process(target=build_workflow, args=(str(config_file), retval))
+        p.start()
+        p.join()
+
+        retcode = p.exitcode or retval.get("return_code", 0)
+        nibabies_wf = retval.get("workflow", None)
+
+    # CRITICAL Load the config from the file. This is necessary because the ``build_workflow``
+    # function executed constrained in a process may change the config (and thus the global
+    # state of NiBabies).
+    config.load(config_file)
+
+    if config.execution.reports_only:
+        sys.exit(int(retcode > 0))
+
+    if nibabies_wf and config.execution.write_graph:
+        nibabies_wf.write_graph(graph2use="colored", format="svg", simple_form=True)
+
+    retcode = retcode or (nibabies_wf is None) * EX_SOFTWARE
+    if retcode != 0:
+        sys.exit(retcode)
+
+    # # Generate boilerplate
+    # with Manager() as mgr:
+    #     from .workflow import build_boilerplate
+
+    #     p = Process(target=build_boilerplate, args=(str(config_file), nibabies_wf))
+    #     p.start()
+    #     p.join()
+
+    # if config.execution.boilerplate_only:
+    #     sys.exit(int(retcode > 0))
+
+    # # Clean up master process before running workflow, which may create forks
+    # gc.collect()
+
+    # Sentry tracking
+    # if sentry_sdk is not None:
+    #     with sentry_sdk.configure_scope() as scope:
+    #         scope.set_tag("run_uuid", config.execution.run_uuid)
+    #         scope.set_tag("npart", len(config.execution.participant_label))
+    #     sentry_sdk.add_breadcrumb(message="nibabies started", level="info")
+    #     sentry_sdk.capture_message("nibabies started", level="info")
+
+    # config.loggers.workflow.log(
+    #     15,
+    #     "\n".join(
+    #         ["nibabies config:"] + ["\t\t%s" % s for s in config.dumps().splitlines()]
+    #     ),
+    # )
+    config.loggers.workflow.log(25, "nibabies started!")
+    errno = 1  # Default is error exit unless otherwise set
+    try:
+        nibabies_wf.run(**config.nipype.get_plugin())
+    except Exception as e:
+        # if not config.execution.notrack:
+        #     from ..utils.sentry import process_crashfile
+
+        #     crashfolders = [
+        #         config.execution.output_dir
+        #         / "nibabies"
+        #         / "sub-{}".format(s)
+        #         / "log"
+        #         / config.execution.run_uuid
+        #         for s in config.execution.participant_label
+        #     ]
+        #     for crashfolder in crashfolders:
+        #         for crashfile in crashfolder.glob("crash*.*"):
+        #             process_crashfile(crashfile)
+
+        #     if "Workflow did not execute cleanly" not in str(e):
+        #         sentry_sdk.capture_exception(e)
+        # config.loggers.workflow.critical("nibabies failed: %s", e)
+        raise
     else:
-        print(f"No workflow for command: {opts.command}", file=sys.stderr)
-        sys.exit(1)
+        config.loggers.workflow.log(25, "nibabies finished successfully!")
+        # if not config.execution.notrack:
+        #     success_message = "nibabies finished without errors"
+        #     sentry_sdk.add_breadcrumb(message=success_message, level="info")
+        #     sentry_sdk.capture_message(success_message, level="info")
 
-    # Run the workflow
-    wf.inputs.inputnode.in_files = opts.input_image
-    wf.base_dir = opts.work_dir
-    nipype_plugin = {"plugin": "Linear"}
-    if opts.nprocs > 1:
-        nipype_plugin["plugin"] = "MultiProc"
-        nipype_plugin["plugin_args"] = {
-            "nproc": opts.nprocs,
-            "raise_insufficient": False,
-            "maxtasksperchild": 1,
-        }
-    wf.base_dir = opts.work_dir
+        # Bother users with the boilerplate only iff the workflow went okay.
+        boiler_file = config.execution.output_dir / "nibabies" / "logs" / "CITATION.md"
+        if boiler_file.exists():
+            if config.environment.exec_env in (
+                "singularity",
+                "docker",
+                "nibabies-docker",
+            ):
+                boiler_file = Path("<OUTPUT_PATH>") / boiler_file.relative_to(
+                    config.execution.output_dir
+                )
+            config.loggers.workflow.log(
+                25,
+                "Works derived from this nibabies execution should include the "
+                f"boilerplate text found in {boiler_file}.",
+            )
 
-    logging.getLogger('nipype.interface').setLevel("DEBUG")
-    wf.run(**nipype_plugin)
+        if config.workflow.run_reconall:
+            from templateflow import api
+            from niworkflows.utils.misc import _copy_any
+
+            dseg_tsv = str(api.get("fsaverage", suffix="dseg", extension=[".tsv"]))
+            _copy_any(
+                dseg_tsv,
+                str(config.execution.output_dir / "nibabies" / "desc-aseg_dseg.tsv"),
+            )
+            _copy_any(
+                dseg_tsv,
+                str(
+                    config.execution.output_dir / "nibabies" / "desc-aparcaseg_dseg.tsv"
+                ),
+            )
+        errno = 0
+    finally:
+        # from niworkflows.reports import generate_reports
+        # from pkg_resources import resource_filename as pkgrf
+
+        # # Generate reports phase
+        # failed_reports = generate_reports(
+        #     config.execution.participant_label,
+        #     config.execution.output_dir,
+        #     config.execution.run_uuid,
+        #     config=pkgrf("nibabies", "data/reports-spec.yml"),
+        #     packagename="nibabies",
+        # )
+        write_derivative_description(
+            config.execution.bids_dir, config.execution.output_dir / "nibabies"
+        )
+        write_bidsignore(config.execution.output_dir / "nibabies")
+
+        # if failed_reports and not config.execution.notrack:
+        #     sentry_sdk.capture_message(
+        #         "Report generation failed for %d subjects" % failed_reports,
+        #         level="error",
+        #     )
+        sys.exit(int((errno + failed_reports) > 0))
+
 
 if __name__ == "__main__":
     raise RuntimeError(
