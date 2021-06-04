7c29e629ac4e909d5afe1b9e671297ee4e214b82
mathiasg <mathiasg@stanford.edu>
2021-06-04 14:16:59 -0400

    RF: Move CiftiDilateInputSpec

--- nibabies/interfaces/workbench.py
+++ nibabies/interfaces/workbench.py
@@ -334,106 +334,6 @@ class CiftiCreateDenseTimeseries(WBCommand):
     _cmd = "wb_command -cifti-create-dense-timeseries"
 
 
-class CiftiDilateInputSpec(CommandLineInputSpec):
-    in_file = File(
-        exists=True,
-        mandatory=True,
-        argstr="%s",
-        position=0,
-        desc="The input CIFTI file",
-    )
-    direction = traits.Enum(
-        "ROW",
-        "COLUMN",
-        mandatory=True,
-        argstr="%s",
-        position=1,
-        desc="Which dimension to dilate along, ROW or COLUMN",
-    )
-    surface_distance = traits.Int(
-        mandatory=True,
-        argstr="%d",
-        position=2,
-        desc="The distance to dilate on surfaces, in mm",
-    )
-    volume_distance = traits.Int(
-        mandatory=True,
-        argstr="%d",
-        position=3,
-        desc="The distance to dilate in the volume, in mm",
-    )
-    out_file = File(
-        name_source=["in_file"],
-        name_template="dilated_%s.nii",
-        keep_extension=True,
-        argstr="%s",
-        position=4,
-        desc="The dilated CIFTI file",
-    )
-    left_surface = File(
-        exists=True,
-        position=5,
-        argstr="-left-surface %s",
-        desc="Specify the left surface to use",
-    )
-    left_corrected_areas = File(
-        exists=True,
-        position=6,
-        requires=["left_surface"],
-        argstr="-left-corrected-areas %s",
-        desc="vertex areas (as a metric) to use instead of computing them from the left surface.",
-    )
-    right_surface = File(
-        exists=True,
-        position=7,
-        argstr="-right-surface %s",
-        desc="Specify the right surface to use",
-    )
-    right_corrected_areas = File(
-        exists=True,
-        position=8,
-        requires=["right_surface"],
-        argstr="-right-corrected-areas %s",
-        desc="vertex areas (as a metric) to use instead of computing them from the right surface",
-    )
-    cerebellum_surface = File(
-        exists=True,
-        position=9,
-        argstr="-cerebellum-surface %s",
-        desc="specify the cerebellum surface to use",
-    )
-    cerebellum_corrected_areas = File(
-        exists=True,
-        position=10,
-        requires=["cerebellum_surface"],
-        argstr="-cerebellum-corrected-areas %s",
-        desc="vertex areas (as a metric) to use instead of computing them from the cerebellum "
-        "surface",
-    )
-    bad_brainordinate_roi = File(
-        exists=True,
-        position=11,
-        argstr="-bad-brainordinate-roi %s",
-        desc="CIFTI dscalar or dtseries file, positive values denote brainordinates to have their "
-        "values replaced",
-    )
-    nearest = traits.Bool(
-        position=12,
-        argstr="-nearest",
-        desc="Use nearest good value instead of a weighted average",
-    )
-    merged_volume = traits.Bool(
-        position=13,
-        argstr="-merged-volume",
-        desc="treat volume components as if they were a single component",
-    )
-    legacy_mode = traits.Bool(
-        position=14,
-        argstr="-legacy-mode",
-        desc="Use the math from v1.3.2 and earlier for weighted dilation",
-    )
-
-
 class CiftiCreateLabelInputSpec(CommandLineInputSpec):
     out_file = File(
         value="dlabel.nii",
@@ -563,6 +463,106 @@ class CiftiCreateLabel(WBCommand):
     input_spec = CiftiCreateLabelInputSpec
     output_spec = CiftiCreateLabelOutputSpec
     _cmd = "wb_command -cifti-create-label"
+
+
+class CiftiDilateInputSpec(CommandLineInputSpec):
+    in_file = File(
+        exists=True,
+        mandatory=True,
+        argstr="%s",
+        position=0,
+        desc="The input CIFTI file",
+    )
+    direction = traits.Enum(
+        "ROW",
+        "COLUMN",
+        mandatory=True,
+        argstr="%s",
+        position=1,
+        desc="Which dimension to dilate along, ROW or COLUMN",
+    )
+    surface_distance = traits.Int(
+        mandatory=True,
+        argstr="%d",
+        position=2,
+        desc="The distance to dilate on surfaces, in mm",
+    )
+    volume_distance = traits.Int(
+        mandatory=True,
+        argstr="%d",
+        position=3,
+        desc="The distance to dilate in the volume, in mm",
+    )
+    out_file = File(
+        name_source=["in_file"],
+        name_template="dilated_%s.nii",
+        keep_extension=True,
+        argstr="%s",
+        position=4,
+        desc="The dilated CIFTI file",
+    )
+    left_surface = File(
+        exists=True,
+        position=5,
+        argstr="-left-surface %s",
+        desc="Specify the left surface to use",
+    )
+    left_corrected_areas = File(
+        exists=True,
+        position=6,
+        requires=["left_surface"],
+        argstr="-left-corrected-areas %s",
+        desc="vertex areas (as a metric) to use instead of computing them from the left surface.",
+    )
+    right_surface = File(
+        exists=True,
+        position=7,
+        argstr="-right-surface %s",
+        desc="Specify the right surface to use",
+    )
+    right_corrected_areas = File(
+        exists=True,
+        position=8,
+        requires=["right_surface"],
+        argstr="-right-corrected-areas %s",
+        desc="vertex areas (as a metric) to use instead of computing them from the right surface",
+    )
+    cerebellum_surface = File(
+        exists=True,
+        position=9,
+        argstr="-cerebellum-surface %s",
+        desc="specify the cerebellum surface to use",
+    )
+    cerebellum_corrected_areas = File(
+        exists=True,
+        position=10,
+        requires=["cerebellum_surface"],
+        argstr="-cerebellum-corrected-areas %s",
+        desc="vertex areas (as a metric) to use instead of computing them from the cerebellum "
+        "surface",
+    )
+    bad_brainordinate_roi = File(
+        exists=True,
+        position=11,
+        argstr="-bad-brainordinate-roi %s",
+        desc="CIFTI dscalar or dtseries file, positive values denote brainordinates to have their "
+        "values replaced",
+    )
+    nearest = traits.Bool(
+        position=12,
+        argstr="-nearest",
+        desc="Use nearest good value instead of a weighted average",
+    )
+    merged_volume = traits.Bool(
+        position=13,
+        argstr="-merged-volume",
+        desc="treat volume components as if they were a single component",
+    )
+    legacy_mode = traits.Bool(
+        position=14,
+        argstr="-legacy-mode",
+        desc="Use the math from v1.3.2 and earlier for weighted dilation",
+    )
 
 
 class CiftiDilateOutputSpec(TraitedSpec):
