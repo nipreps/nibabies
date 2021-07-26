10f1f455429d79a1ab013415234a4f317d7daa57
mathiasg <mathiasg@stanford.edu>
2021-07-26 14:17:50 -0400

    STY: Black

--- nibabies/interfaces/tests/test_nibabel.py
+++ nibabies/interfaces/tests/test_nibabel.py
@@ -6,9 +6,11 @@ import pytest
 
 from ..nibabel import MergeROIs
 
+
 @pytest.fixture
 def create_roi(tmp_path):
     files = []
+
     def _create_roi(affine, img_data, roi_index):
         img_data[tuple(roi_index)] = 1
         nii = nb.Nifti1Image(img_data, affine)
@@ -16,25 +18,49 @@ def create_roi(tmp_path):
         files.append(filename)
         nii.to_filename(filename)
         return filename
+
     yield _create_roi
 
     for f in files:
         f.unlink()
 
+
 # create a slightly off affine
 bad_affine = np.eye(4)
 bad_affine[0, -1] = -1
 
-@pytest.mark.parametrize("affine,data,roi_index,error,err_message", [
-    (np.eye(4), np.zeros((2,2,2,2), dtype=int), [1, 0], None, None),
-    (np.eye(4), np.zeros((2,2,3,2), dtype=int), [1, 0], True, "Mismatch in image shape"),
-    (bad_affine, np.zeros((2,2,2,2), dtype=int), [1, 0], True, "Mismatch in affine"),
-    (np.eye(4), np.zeros((2,2,2,2), dtype=int), [0, 0, 0], True, "Overlapping ROIs"),
-])
+
+@pytest.mark.parametrize(
+    "affine, data, roi_index, error, err_message",
+    [
+        (np.eye(4), np.zeros((2, 2, 2, 2), dtype=int), [1, 0], None, None),
+        (
+            np.eye(4),
+            np.zeros((2, 2, 3, 2), dtype=int),
+            [1, 0],
+            True,
+            "Mismatch in image shape",
+        ),
+        (
+            bad_affine,
+            np.zeros((2, 2, 2, 2), dtype=int),
+            [1, 0],
+            True,
+            "Mismatch in affine",
+        ),
+        (
+            np.eye(4),
+            np.zeros((2, 2, 2, 2), dtype=int),
+            [0, 0, 0],
+            True,
+            "Overlapping ROIs",
+        ),
+    ],
+)
 def test_merge_rois(tmpdir, create_roi, affine, data, roi_index, error, err_message):
     tmpdir.chdir()
-    roi0 = create_roi(np.eye(4), np.zeros((2,2,2,2), dtype=int), [0, 0])
-    roi1 = create_roi(np.eye(4), np.zeros((2,2,2,2), dtype=int), [0, 1])
+    roi0 = create_roi(np.eye(4), np.zeros((2, 2, 2, 2), dtype=int), [0, 0])
+    roi1 = create_roi(np.eye(4), np.zeros((2, 2, 2, 2), dtype=int), [0, 1])
     test_roi = create_roi(affine, data, roi_index)
 
     merge = MergeROIs(in_files=[roi0, roi1, test_roi])
