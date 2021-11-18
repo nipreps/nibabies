e28e82d14aa00878769ffec40e838baea6bf2988
mathiasg <mathiasg@stanford.edu>
2021-11-18 18:34:11 -0500

    ENH: Add utility function to generate sample BIDS directory

--- nibabies/utils/bids.py
+++ nibabies/utils/bids.py
@@ -304,3 +304,82 @@ def validate_input_dir(exec_env, bids_dir, participant_label):
             subprocess.check_call(["bids-validator", str(bids_dir), "-c", temp.name])
         except FileNotFoundError:
             print("bids-validator does not appear to be installed", file=sys.stderr)
+
+
+def generate_bids_skeleton(location, _bids_dict):
+    """
+    Converts a BIDS directory in dictionary form to a file structure.
+    """
+    from copy import deepcopy
+
+    # ensure dictionary remains unaltered
+    bids_dict = _bids_dict.copy()
+
+    root = Path(location).absolute()
+    root.mkdir(parents=True, exist_ok=True)
+
+    desc = bids_dict.pop("dataset_description", None)
+    if desc is None:
+        # default description
+        desc = {
+            "Name": "Default",
+            "BIDSVersion": "1.6.0"
+        }
+    to_json(root / "dataset_description.json", desc)
+
+    cached_subject_data = None
+    for subject, sessions in bids_dict.items():
+        bids_subject = subject if subject.startswith("sub-") else f"sub-{subject}"
+        subj_path = root / bids_subject
+        subj_path.mkdir(exist_ok=True)
+
+        if sessions == "*":  # special case to copy previous subject data
+            sessions = cached_subject_data.copy()
+
+        if isinstance(sessions, dict):  # single session
+            sessions.update({"session": None})
+            sessions = [sessions]
+
+        cached_subject_data = deepcopy(sessions)
+        for session in sessions:
+
+            ses_name = session.get("session")
+            if ses_name is not None:
+                bids_session = ses_name if ses_name.startswith("ses-") else f"ses-{ses_name}"
+                bids_prefix = f"{bids_subject}_{bids_session}"
+                curr_path = subj_path / bids_session
+                curr_path.mkdir(exist_ok=True)
+            else:
+                bids_prefix = bids_subject
+                curr_path = subj_path
+
+            # create modalities
+            for modality, files in session.items():
+                if modality == "session":
+                    continue
+                modality_path = curr_path / modality
+                modality_path.mkdir(exist_ok=True)
+
+                if isinstance(files, dict):  # single file / metadata combo
+                    files = [files]
+
+                for bids_file in files:
+                    metadata = bids_file.pop("metadata", None)
+                    suffix = bids_file.pop("suffix")
+                    entities = combine_entities(**bids_file)
+                    nii_file = modality_path / f"{bids_prefix}{entities}_{suffix}.nii.gz"
+                    nii_file.touch()
+
+                    if metadata is not None:
+                        nii_metadata = nii_file.parent / nii_file.name.replace('nii.gz', 'json')
+                        to_json(nii_metadata, metadata)
+
+
+def to_json(file, _dict):
+    Path(file).write_text(json.dumps(_dict))
+
+
+def combine_entities(**entities):
+    if not entities:
+        return ""
+    return "_" + "_".join([f"{lab}-{val}" for lab, val in entities.items()])
