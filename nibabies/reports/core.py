bb6f8fa6e1e4cdc5cb80888ba7b38ca803c64d0d
mathiasg <mathiasg@stanford.edu>
2022-01-21 11:12:49 -0500

    FIX: Patch niworkflows `Report` out_filename generation

--- nibabies/reports/core.py
+++ nibabies/reports/core.py
@@ -1,9 +1,43 @@
+from itertools import product
 from pathlib import Path
-
+from pkg_resources import resource_filename as pkgrf
 from niworkflows.reports.core import Report as _Report
 
 
 class Report(_Report):
+    # niworkflows patch to preserve `out_filename` even if subject_id is present
+    def __init__(
+        self,
+        out_dir,
+        run_uuid,
+        config=None,
+        out_filename=None,
+        packagename=None,
+        reportlets_dir=None,
+        subject_id=None,
+    ):
+        self.root = Path(reportlets_dir or out_dir)
+
+        # Initialize structuring elements
+        self.sections = []
+        self.errors = []
+        self.out_dir = Path(out_dir)
+        self.run_uuid = run_uuid
+        self.packagename = packagename
+        self.subject_id = subject_id
+        if subject_id is not None:
+            self.subject_id = subject_id[4:] if subject_id.startswith("sub-") else subject_id
+            # ensure set output filename is preserved
+            if not out_filename:
+                out_filename = f"sub-{self.subject_id}.html"
+
+        self.out_filename = out_filename or "report.html"
+
+        # Default template from niworkflows
+        self.template_path = Path(pkgrf("niworkflows", "reports/report.tpl"))
+        self._load_config(Path(config or pkgrf("niworkflows", "reports/default.yml")))
+        assert self.template_path.exists()
+
     # TODO: Upstream ``Report._load_config`` to niworkflows
     def _load_config(self, config):
         from yaml import safe_load as load
@@ -33,6 +67,7 @@ def run_reports(
     subject_label,
     run_uuid,
     config=None,
+    out_filename='report.html',
     reportlets_dir=None,
     packagename=None,
 ):
@@ -43,6 +78,7 @@ def run_reports(
         out_dir,
         run_uuid,
         config=config,
+        out_filename=out_filename,
         subject_id=subject_label,
         packagename=packagename,
         reportlets_dir=reportlets_dir,
@@ -50,23 +86,36 @@ def run_reports(
 
 
 def generate_reports(
-    subject_list, output_dir, run_uuid, config=None, work_dir=None, packagename=None
+    subject_list,
+    sessions_list,
+    output_dir,
+    run_uuid,
+    config=None,
+    work_dir=None,
+    packagename=None,
 ):
     """Execute run_reports on a list of subjects."""
     reportlets_dir = None
     if work_dir is not None:
         reportlets_dir = Path(work_dir) / "reportlets"
-    report_errors = [
-        run_reports(
-            output_dir,
-            subject_label,
-            run_uuid,
-            config=config,
-            packagename=packagename,
-            reportlets_dir=reportlets_dir,
+
+    report_errors = []
+    for subject_label, session in product(subject_list, sessions_list):
+        html_report = f"sub-{subject_label}"
+        if session:
+            html_report += f"_ses-{session}"
+        html_report += ".html"
+        report_errors.append(
+            run_reports(
+                output_dir,
+                subject_label,
+                run_uuid,
+                config=config,
+                out_filename=html_report,
+                packagename=packagename,
+                reportlets_dir=reportlets_dir,
+            )
         )
-        for subject_label in subject_list
-    ]
 
     errno = sum(report_errors)
     if errno:
