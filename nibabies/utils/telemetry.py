5590e462114aa9638cdba04d47e37aa408470930
mathiasg <mathiasg@stanford.edu>
2022-12-07 12:30:47 -0500

    MAINT: Improve migas telemetry

--- nibabies/utils/telemetry.py
+++ nibabies/utils/telemetry.py
@@ -3,10 +3,10 @@ import migas
 from .. import __version__, config
 
 
-def setup_migas(init_ping: bool = True) -> None:
+def setup_migas(init: bool = True) -> None:
     """
     Prepare the migas python client to communicate with a migas server.
-    If `init_ping` is `True`, send an initial breadcrumb.
+    If ``init`` is ``True``, send an initial breadcrumb.
     """
     # generate session UUID from generated run UUID
     session_id = None
@@ -14,14 +14,14 @@ def setup_migas(init_ping: bool = True) -> None:
         session_id = config.execution.run_uuid.split('_', 1)[-1]
 
     migas.setup(session_id=session_id)
-    if init_ping:
+    if init:
         # send initial status ping
-        ping_migas(status='pending')
+        send_breadcrumb(status='R', status_desc='workflow start')
 
 
-def ping_migas(*, status: str) -> dict:
+def send_breadcrumb(**kwargs) -> dict:
     """
     Communicate with the migas telemetry server. This requires `migas.setup()` to be called.
     """
-    res = migas.add_project("nipreps/nibabies", __version__, status=status)
+    res = migas.add_project("nipreps/nibabies", __version__, **kwargs)
     return res
