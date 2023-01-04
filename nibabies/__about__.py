e9a6d63cdd8c12dad487dcda7e676babc5fa9a5e
mathiasg <mathiasg@stanford.edu>
2023-01-04 16:14:14 -0500

    MAINT: Fix version import

--- nibabies/__about__.py
+++ nibabies/__about__.py
@@ -1,10 +1,11 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Base module variables."""
-from ._version import get_versions
 
-__version__ = get_versions()["version"]
-del get_versions
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ == "0+unknown"
 
 __org__ = "nipreps"
 __packagename__ = "nibabies"
