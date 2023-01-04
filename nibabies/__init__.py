5345a47c615bb0cc0e67bd239323212508bd0d00
mathiasg <mathiasg@stanford.edu>
2023-01-04 15:42:58 -0500

    MAINT: Move all project metadata to pyproject.toml

--- nibabies/__init__.py
+++ nibabies/__init__.py
@@ -1,16 +1,4 @@
-from . import _version
-
-__version__ = _version.get_versions()['version']
-
-# Silence PyBIDS warning for extension entity behavior
-# Can be removed once minimum PyBIDS dependency hits 0.14
 try:
-    import bids
-    from packaging.version import Version
-
-    if Version(bids.__version__) < Version("0.14.0"):
-        bids.config.set_option("extension_initial_dot", True)
-except (ImportError, ValueError):
-    pass
-else:
-    del bids
+    from ._version import __version__
+except ImportError:
+    __version__ == "0+unknown"
