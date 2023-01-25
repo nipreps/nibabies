f35008316bb3601db534b4a643a1845de2c49099
Thomas Madison <56737848+madisoth@users.noreply.github.com>
2023-01-25 10:52:00 -0800

    Remap labels from FreeSurfer aseg to MCRIB  (#270)
    * use MapLabel interface to remap aseg labels to mcrib-compatible
    
    * STY: black
    
    * Update nibabies/workflows/anatomical/segmentation.py
    
    Co-authored-by: Mathias Goncalves <goncalves.mathias@gmail.com>
    
    * Update nibabies/workflows/anatomical/segmentation.py
    
    Co-authored-by: Mathias Goncalves <goncalves.mathias@gmail.com>
    
    * Update segmentation.py
    
    add suggestions from PR review
    
    * Update segmentation.py
    
    formatting
    
    * Update segmentation.py
    
    define aseg2mcrib dict before using
    
    Co-authored-by: Mathias Goncalves <goncalves.mathias@gmail.com>

--- nibabies/workflows/anatomical/base.py
+++ nibabies/workflows/anatomical/base.py
@@ -146,6 +146,7 @@ BIDS dataset."""
                 "surfaces",
                 "morphometrics",
                 "anat_aseg",
+                "anat_mcrib",
                 "anat_aparc",
                 "template",
             ]
