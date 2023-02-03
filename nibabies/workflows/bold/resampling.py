df64ec5dd4a707a07dbd72115e603ecac7172068
madisoth <tmadison@umn.edu>
2023-02-02 18:08:33 -0600

    run black

--- nibabies/workflows/bold/resampling.py
+++ nibabies/workflows/bold/resampling.py
@@ -36,8 +36,8 @@ def init_bold_surf_wf(
 
     If --project-goodvoxels is used, a "goodvoxels" BOLD mask, as described in [@hcppipelines],
     is generated and applied to the functional image before sampling to surface. After sampling,
-    empty vertices are filled by dilating in data from the nearest "good" vertex, within a 
-    radius of 10 mm (as measured on the target midthickness surface). 
+    empty vertices are filled by dilating in data from the nearest "good" vertex, within a
+    radius of 10 mm (as measured on the target midthickness surface).
     Outputs are in GIFTI format.
 
     Workflow Graph
