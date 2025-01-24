93cc325e2942ec61f3cdc009d66138755e56117a
mathiasg <mathiasg@stanford.edu>
2025-01-24 00:21:58 -0500

    ENH: Output T1<->T2 coregistration

--- nibabies/workflows/anatomical/fit.py
+++ nibabies/workflows/anatomical/fit.py
@@ -35,6 +35,7 @@ from smriprep.workflows.surfaces import (
 )
 
 from nibabies import config
+from nibabies.interfaces import DerivativesDataSink
 from nibabies.workflows.anatomical.brain_extraction import init_infant_brain_extraction_wf
 from nibabies.workflows.anatomical.outputs import init_anat_reports_wf
 from nibabies.workflows.anatomical.preproc import init_anat_preproc_wf, init_csf_norm_wf
@@ -741,36 +742,31 @@ def init_infant_anat_fit_wf(
             probmap=probmap,
         )
 
-        # TODO: Currently the XFMs are transform0GenericAffine.mat, transform1Warp.nii.gz
-        # The coregistration should be changed to instead save
-        # 'composite_transform' and 'inverse_composite_transform'
-        # from antsRegistration (single h5 files)
-        #
-        # ds_t1w2t2w_xfm = pe.Node(
-        #     DerivativesDataSink(
-        #         base_directory=output_dir,
-        #         to='T2w',
-        #         mode='image',
-        #         suffix='xfm',
-        #         dismiss_entites=('desc', 'echo'),
-        #         **{'from': 'T1w'}
-        #     ),
-        #     name='ds_t1w2t2w_xfm',
-        #     run_without_submitting=True,
-        # )
+        ds_t1w2t2w_xfm = pe.Node(
+            DerivativesDataSink(
+                base_directory=output_dir,
+                to='T2w',
+                mode='image',
+                suffix='xfm',
+                dismiss_entites=('desc', 'echo'),
+                **{'from': 'T1w'},
+            ),
+            name='ds_t1w2t2w_xfm',
+            run_without_submitting=True,
+        )
 
-        # ds_t2w2t1w_xfm = pe.Node(
-        #     DerivativesDataSink(
-        #         base_directory=output_dir,
-        #         to='T1w',
-        #         mode='image',
-        #         suffix='xfm',
-        #         dismiss_entites=('desc', 'echo'),
-        #         **{'from': 'T2w'}
-        #     ),
-        #     name='ds_t2w2t1w_xfm',
-        #     run_without_submitting=True,
-        # )
+        ds_t2w2t1w_xfm = pe.Node(
+            DerivativesDataSink(
+                base_directory=output_dir,
+                to='T1w',
+                mode='image',
+                suffix='xfm',
+                dismiss_entites=('desc', 'echo'),
+                **{'from': 'T2w'},
+            ),
+            name='ds_t2w2t1w_xfm',
+            run_without_submitting=True,
+        )
 
         workflow.connect([
             (t1w_validate, coregistration_wf, [
@@ -780,18 +776,18 @@ def init_infant_anat_fit_wf(
                 ('t2w_preproc', 'inputnode.in_t2w'),
                 ('t2w_mask', 'inputnode.in_mask'),
             ]),
-            # (coregistration_wf, ds_t1w2t2w_xfm, [
-            #     ('outputnode.t1w2t2w_xfm', 'in_file'),
-            # ]),
-            # (sourcefile_buffer, ds_t1w2t2w_xfm, [
-            #     ('t1w_source_files', 'source_file'),
-            # ]),
-            # (coregistration_wf, ds_t2w2t1w_xfm, [
-            #     ('outputnode.t2w2t1w_xfm', 'in_file'),
-            # ]),
-            # (sourcefile_buffer, ds_t2w2t1w_xfm, [
-            #     ('t2w_source_files', 'source_file'),
-            # ]),
+            (coregistration_wf, ds_t1w2t2w_xfm, [
+                ('outputnode.t1w2t2w_xfm', 'in_file'),
+            ]),
+            (sourcefile_buffer, ds_t1w2t2w_xfm, [
+                ('t1w_source_files', 'source_file'),
+            ]),
+            (coregistration_wf, ds_t2w2t1w_xfm, [
+                ('outputnode.t2w2t1w_xfm', 'in_file'),
+            ]),
+            (sourcefile_buffer, ds_t2w2t1w_xfm, [
+                ('t2w_source_files', 'source_file'),
+            ]),
             (coregistration_wf, coreg_buffer, [
                 ('outputnode.t1w2t2w_xfm', 't1w2t2w_xfm'),
                 ('outputnode.t2w2t1w_xfm', 't2w2t1w_xfm'),
