8f26bfb599422bdfb152ace73f5c3199b3bc4649
Mathias Goncalves <goncalves.mathias@gmail.com>
2025-08-27 22:13:43 -0400

    fix: output to nibabies dir

--- nibabies/workflows/base.py
+++ nibabies/workflows/base.py
@@ -836,7 +836,7 @@ def clean_datasinks(workflow: pe.Workflow) -> pe.Workflow:
     for node in workflow.list_node_names():
         if node.split('.')[-1].startswith('ds_'):
             workflow.get_node(node).interface.out_path_base = ''
-            workflow.get_node(node).interface.inputs.base_directory = config.execution.output_dir
+            workflow.get_node(node).interface.inputs.base_directory = config.execution.nibabies_dir
 
             if config.execution.output_layout == 'multiverse':
                 workflow.get_node(node).interface.inputs.hash = config.execution.parameters_hash
