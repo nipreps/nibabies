3ec64a81c66c5cf982c5688296f8bffbb3b0a173
Mathias Goncalves <goncalves.mathias@gmail.com>
2026-04-23 13:12:05 -0400

    rf: use IsFile type

--- nibabies/cli/parser.py
+++ nibabies/cli/parser.py
@@ -87,15 +87,6 @@ def _build_parser():
             raise parser.error(f'Path should point to a file (or symlink of file): <{path}>.')
         return path
 
-    def _is_file_or_none_as_str(path, parser):
-        """Ensure a given path exists and it is a file."""
-        if path is None or path == "":
-            return None
-        path = _path_exists(path, parser)
-        if not path.is_file():
-            raise parser.error(f'Path should point to a file (or symlink of file): <{path}>.')
-        return str(path.resolve())
-
     def _min_one(value, parser):
         """Ensure an argument is not lower than 1."""
         value = int(value)
@@ -189,7 +180,6 @@ def _build_parser():
     PathExists = partial(_path_exists, parser=parser)
     DirNotEmpty = partial(_dir_not_empty, parser=parser)
     IsFile = partial(_is_file, parser=parser)
-    IsFileOrNoneAsStr = partial(_is_file_or_none_as_str, parser=parser)
     PositiveInt = partial(_min_one, parser=parser)
     BIDSFilter = partial(_bids_filter, parser=parser)
     SliceTimeRef = partial(_slice_time_ref, parser=parser)
@@ -580,7 +570,7 @@ Useful for further Tedana processing post-NiBabies.""",
     g_fmap.add_argument(
         '--topup-config',
         metavar='FILE',
-        type=IsFileOrNoneAsStr,
+        type=IsFile,
         default=None,
         help='path to a custom topup config file',
     )
